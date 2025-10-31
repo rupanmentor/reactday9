7dbc76ae722a71602ce91a2a65340b7f2785d8d1
rupanmentor <rupanguvimentor@gmail.com>
2025-10-31 21:00:21 +0530

    Delete completed

--- src/Pages/Edit.jsx
+++ src/Pages/Edit.jsx
@@ -2,49 +2,44 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Edit = ({id}) => {
-   const [formData, setFormData] = useState({
-      productName : "",
-      productPrice:"",
-      productDescription:"",
-      productImage:""
-    });
-    const navigate = useNavigate();
-   
-    useEffect(()=>{
-        fetchData();
-    },[])
-   
-    const fetchData = async() =>{
-      try {
-        const response = await axios.get(`https://6904bdfb6b8dabde4964ef27.mockapi.io/products/${id}`)
-        setFormData(response.data)
+const Edit = ({ id }) => {
+  const [formData, setFormData] = useState({
+    productName: "",
+    productPrice: "",
+    productDescription: "",
+    productImage: "",
+  });
+  const navigate = useNavigate();
 
-      } catch (error) {
-        console.log(error);
-        
-      }
-    }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-
-
-
- const handleSubmit = async (e) => {
-    e.preventDefault();
+  const fetchData = async () => {
     try {
-      await axios.put(`https://6904bdfb6b8dabde4964ef27.mockapi.io/products/${id}`,formData)
-      navigate("/")
+      const response = await axios.get(
+        `https://6904bdfb6b8dabde4964ef27.mockapi.io/products/${id}`
+      );
+      setFormData(response.data);
     } catch (error) {
       console.log(error);
-      
     }
-   
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.put(
+        `https://6904bdfb6b8dabde4964ef27.mockapi.io/products/${id}`,
+        formData
+      );
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-
-
-    const handleChange = (e) => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((previousData) => ({
       ...previousData,
@@ -52,7 +47,7 @@ const Edit = ({id}) => {
     }));
   };
   return (
-     <>
+    <>
       <form className="max-w-sm mx-auto mt-5" onSubmit={handleSubmit}>
         <div className="mb-5">
           <label
@@ -130,7 +125,7 @@ const Edit = ({id}) => {
           type="submit"
           className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
         >
-         Update
+          Update
         </button>
       </form>
     </>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,13 +2,14 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Home = ({setId}) => {
+const Home = ({ setId }) => {
   const [data, setData] = useState([]);
+  const [deleteData, setDeleteData] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [deleteData]);
 
   const fetchData = async () => {
     try {
@@ -21,11 +22,21 @@ const Home = ({setId}) => {
     }
   };
 
-  const handleEdit = (id)=>{
-      setId(id)
-      navigate(`/edit/${id}`)
-  }
+  const handleEdit = (id) => {
+    setId(id);
+    navigate(`/edit/${id}`);
+  };
 
+  const handleDelete = async (id) => {
+    try {
+      const response = await axios.delete(
+        `https://6904bdfb6b8dabde4964ef27.mockapi.io/products/${id}`
+      );
+      setDeleteData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="flex flex-1 flex-wrap gap-6 mt-5">
@@ -34,7 +45,11 @@ const Home = ({setId}) => {
           <div key={ele.productId}>
             <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
               <a href="#">
-                <img className="rounded-t-lg" src={ele.productImage} alt={ele.productName} />
+                <img
+                  className="rounded-t-lg"
+                  src={ele.productImage}
+                  alt={ele.productName}
+                />
               </a>
               <div className="p-5">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -49,13 +64,14 @@ const Home = ({setId}) => {
                 <button
                   type="button"
                   className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                  onClick={()=>handleEdit(ele.productId)}
+                  onClick={() => handleEdit(ele.productId)}
                 >
                   Edit
                 </button>
                 <button
                   type="button"
                   className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                  onClick={() => handleDelete(ele.productId)}
                 >
                   Delete
                 </button>
